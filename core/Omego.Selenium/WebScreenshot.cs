b628151041929f6012f6e93968617d4cba7607ce
Oleg Kleyman <Omego2K@gmail.com>
2016-05-14 22:26:12 -0400

    throw exception when screenshot is null

--- core/Omego.Selenium/WebScreenshot.cs
+++ core/Omego.Selenium/WebScreenshot.cs
@@ -2,12 +2,16 @@ using OpenQA.Selenium;
 
 namespace Omego.Selenium
 {
+    using System;
+
     public class WebScreenshot
     {
         private readonly Screenshot screenshot;
 
         public WebScreenshot(Screenshot screenshot)
         {
+            if (screenshot == null) throw new ArgumentNullException(nameof(screenshot));
+
             this.screenshot = screenshot;
         }
 
--- tests/unit/Omego.Selenium.Tests.Unit/WebScreenshotTests.cs
+++ tests/unit/Omego.Selenium.Tests.Unit/WebScreenshotTests.cs
@@ -1,6 +1,50 @@
 ﻿namespace Omego.Selenium.Tests.Unit
 {
+    using System;
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using OpenQA.Selenium;
+
+    using Xunit;
+
     public class WebScreenshotTests
     {
+        [CLSCompliant(false)]
+        [Theory]
+        [MemberData(nameof(WebScreenshotTestData.ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalid),
+            MemberType = typeof(WebScreenshotTestData))]
+        public void ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalid(
+            Screenshot screenshot,
+            string expectedParameterName,
+            string expectedMessage,
+            Type expectedException)
+
+        {
+            Action constructor = () => new WebScreenshot(screenshot);
+
+            constructor.ShouldThrow<ArgumentException>()
+                .WithMessage(expectedMessage)
+                .Where(
+                    exception => exception.ParamName == expectedParameterName,
+                    "the parameter name should be of the problematic parameter")
+                .And.Should()
+                .BeOfType(expectedException);
+        }
+
+        public static class WebScreenshotTestData
+        {
+            public static IEnumerable<object[]> ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalid
+                =>
+                    new List<object[]>
+                        {
+                            new object[]
+                                {
+                                    null, "screenshot", "Value cannot be null.\r\nParameter name: screenshot",
+                                    typeof(ArgumentNullException)
+                                }
+                        };
+        }
     }
 }
