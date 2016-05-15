9c4e7e0d2946b2887abc9ae8bb53cc828f168017
Oleg Kleyman <Omego2K@gmail.com>
2016-05-14 23:26:24 -0400

    add test for AsByteArray property

--- tests/unit/Omego.Selenium.Tests.Unit/WebScreenshotTests.cs
+++ tests/unit/Omego.Selenium.Tests.Unit/WebScreenshotTests.cs
@@ -40,11 +40,8 @@
         [Fact]
         public void SaveToShouldSaveToTheFileSystem()
         {
-            var screenshot =
-                new WebScreenshot(
-                    new Screenshot(
-                        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAAECAIAAADAusJtAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8Y"
-                        + "QUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAAQSURBVBhXY/j//z8C//8PAF+wC/WcqJPUAAAAAElFTkSuQmCC"));
+            var screenshot = GetWebScreenshot("iVBORw0KGgoAAAANSUhEUgAAAAEAAAAECAIAAADAusJtAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8Y"
+                                              + "QUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAAQSURBVBhXY/j//z8C//8PAF+wC/WcqJPUAAAAAElFTkSuQmCC");
 
             var fileSystem = new MockFileSystem();
             var imageTarget = new ImageTarget("test", "test.bmp", ImageFormat.Bmp);
@@ -54,6 +51,19 @@
             fileSystem.File.Exists(imageTarget.CombinedPath).Should().BeTrue();
         }
 
+        [Fact]
+        public void AsByteArrayShouldReturnTheSerializedImageArray()
+        {
+            var screenshot = GetWebScreenshot("test");
+
+            screenshot.AsByteArray.ShouldBeEquivalentTo(new byte[] { 181, 235, 45 });
+        }
+
+        private static WebScreenshot GetWebScreenshot(string base64EncodedScreenshot)
+        {
+            return new WebScreenshot(new Screenshot(base64EncodedScreenshot));
+        }
+        
         public static class WebScreenshotTestData
         {
             public static IEnumerable<object[]> ConstructorShouldThrowArgumentExceptionWhenArgumentsAreInvalid
